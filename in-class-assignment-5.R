e6319737536cf1ca6ec5a0f5226badb7cd23e0c3
Jayques Nelson <jayquesnelson10@gmail.com>
2023-11-03 15:20:28 -0400

    Add files via upload

---
+++ in-class-assignment-5.R
@@ -0,0 +1,10 @@
+#Nov 3, 2023; author: Jayques Nelson; Chi-square test
+
+data_frame <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+
+chisq.test(data_frame$new_cases, data_frame$icu_patients_per_million, correct=FALSE)
+
+ Pearson's Chi-squared test
+
+data:  data_frame$new_cases and data_frame$icu_patients_per_million
+X-squared = 186153995, df = 131323400, p-value < 2.2e-16
